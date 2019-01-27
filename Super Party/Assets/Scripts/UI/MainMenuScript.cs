51fbabc89b3bd2e0f101dc92f85a7fc9e32d39e1
Carlosesgenial3 <47064395+Carlosesgenial3@users.noreply.github.com>
2019-01-26 21:23:58 -0600

    Menu principal modificado ligeramente

--- Super Party/Assets/Scripts/UI/MainMenuScript.cs
+++ Super Party/Assets/Scripts/UI/MainMenuScript.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour {
+
+    [SerializeField] Button Play;
+    [SerializeField] Button Option;
+    [SerializeField] Button Exit;
 
     public void PlayButtonPressed()
     {

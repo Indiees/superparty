5f3fb47feb0a762863da0b00328c0c9874daf47b
Carlosesgenial3 <47064395+Carlosesgenial3@users.noreply.github.com>
2019-01-26 18:32:59 -0600

    Se agregaron las escenas de Pantalla principal, opciones, lobby y pantalla de juego terminado

---
+++ Super Party/Assets/Scripts/UI/LobbyScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LobbyScript : MonoBehaviour {
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
